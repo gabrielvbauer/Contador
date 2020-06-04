44f5d01906d3f3e1e16b9798d808b864ceaa5788
Gabriel <gabriel.bauer9@hotmail.com>
2020-06-04 13:20:06 -0300

    Considerando passo = 1 caso seja 0

--- js/functions.js
+++ js/functions.js
@@ -6,17 +6,24 @@ function contar() {
     var res = document.getElementById('res')
 
     if (init.value.length == '' || final.value.length == '' || passo.value == '') {
-        window.alert('[ERRO] Digite um valor válido')
+        res.innerHTML = `Impossível contar.`
     } else {
         res.innerHTML = `Contando: <br>`
         var initvalue = Number(init.value);
         var finalvalue = Number(final.value);
         var passovalue = Number(passo.value);
 
+        if (passovalue == 0) {
+            alert('Passo inválido. Considerando passo como 1');
+            passovalue = 1;
+        }
+
+        //CONTAGEM DECRESCENTE
         if (initvalue > finalvalue) {
             for (calc = initvalue; calc >= finalvalue; calc -= passovalue) {
                 res.innerHTML += `${calc} `;
             }
+            //CONTAGEM CRESCENTE
         } else {
             for (calc2 = initvalue; calc2 <= finalvalue; calc2 += passovalue) {
                 res.innerHTML += `${calc2} `;
